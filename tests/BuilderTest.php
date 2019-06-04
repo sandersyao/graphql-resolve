16b5936d7c4954d8e4acacd7f39025d1590e8a10
pikaqiu <pikaqiu@kdd.com>
2019-06-04 16:49:34 +0800

    完善Builder测试用例

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @todo 完善Builder测试用例
+ * Builder测试用例
  */
 namespace GraphQLResolve\Tests;
 
@@ -11,8 +11,26 @@ use PHPUnit\Framework\TestCase;
 
 class BuilderTest extends TestCase
 {
+    /**
+     * 测试正常工作状态
+     *
+     * @covers \GraphQLResolve\Builders\AbstractBuilder::fetch
+     */
     public function testBuilderCreate()
     {
+        $callbackA  = function () {};
+        $callbackB  = function () {};
+        $builder    = A::getInstance()
+            ->fields($callbackA)
+            ->resolve($callbackB);
+        $object     = $builder->fetch();
+
+        $this->assertEquals($callbackA, $builder->fields());
+        $this->assertEquals($callbackB, $builder->resolve());
+        $this->assertEquals($object, [
+            'fields'    => $callbackA,
+            'resolve'   => $callbackB,
+        ]);
     }
 }
 
@@ -21,13 +39,13 @@ class A extends AbstractBuilder
     public function getItems(): array
     {
         return  [
-            'a',
-            'b',
+            'fields'    => 'required|type:array,callable',
+            'resolve'   => 'type:callable',
         ];
     }
 
     public function fetch(...$args)
     {
-        return [];
+        return  $this->options();
     }
 }
